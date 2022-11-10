0bf3d8c1ddeb07db025da3cdff557b78a5838e09
andrey <rodandr13@gmail.com>
2022-11-10 22:46:14 +0300

    feature: create fields

--- smartphones/items.py
+++ smartphones/items.py
@@ -7,6 +7,5 @@ import scrapy
 
 
 class SmartphonesItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    os = scrapy.Field()
+    version = scrapy.Field()
