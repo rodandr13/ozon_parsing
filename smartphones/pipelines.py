b60aeda2f313155f80125fd6051ecc0aca17afea
andrey <rodandr13@gmail.com>
2022-11-10 23:13:50 +0300

    feature: add print result models

--- smartphones/pipelines.py
+++ smartphones/pipelines.py
@@ -2,8 +2,8 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
-
+import json
+import pandas as pd
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 
@@ -11,3 +11,10 @@ from itemadapter import ItemAdapter
 class SmartphonesPipeline:
     def process_item(self, item, spider):
         return item
+
+    def close_spider(self, spider):
+        with open("result.json", "r", encoding="utf-8") as file:
+            result = json.load(file)
+        df = pd.DataFrame(result)
+        print(df.value_counts())
+
